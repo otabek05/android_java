e4467d9270b4bd88c46f0f374b7374bdfd28a8fd
otabek05 <otabektoshmukhammedov@gmail.com>
2024-11-13 17:36:22 +0900

    Implicit Intents has been introduced

--- app/src/main/java/com/daeryuk/fsapp/MainActivity.java
+++ app/src/main/java/com/daeryuk/fsapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.daeryuk.fsapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,30 +22,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Intents facilitate communication between different component as well as between different applications
-
-        // Type of intents
-        // 1- Explicit Intents  ===> used specific components within the application, by specifying component class name
-        // 2- Implicit Intents
-
         Button btn = findViewById(R.id.button);
-
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                moveToSecond();
+                openCamera();
             }
         });
+
         Toast.makeText(this, "onCreate method has been called", Toast.LENGTH_SHORT).show();
 
     }
+
+    // Intents facilitate communication between different component as well as between different applications
+
+    // Type of intents
+    // 1- Explicit Intents  ===> used specific components within the application, by specifying component class name
+    // 2- Implicit Intents  ===> used for extenal services in the phone such as browser or camera
+
+   // Explicit Intents
 
     public void moveToSecond() {
         Intent intent = new Intent(this, Second.class);
         startActivity(intent);
     }
 
+    // Implicit Intents
 
+    public void openNewPage() {
+        Uri webpage = Uri.parse("https://daeryuk.org");
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void openCamera()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+
+   // Life Cycles execpt onCreate()
     @Override
     protected void onStart() {
         super.onStart();
